6e920df2cfec167f6426bc4e2f452120b1c4c885
Martin <xvalac12@stud.fit.vutbr.cz>
2022-04-10 11:28:15 +0200

    Change name of Tk() variable from calc to calc_gui, because there is a file named calc.py

--- src/gui.py
+++ src/gui.py
@@ -7,12 +7,13 @@
 import functions
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 
 # elementary settings of window
-calc = Tk()
-calc.geometry('306x329')
-calc.resizable(False, False)
-calc.title('Calculator G.I.I.T.')
+calc_gui = Tk()
+calc_gui.geometry('306x329')
+calc_gui.resizable(False, False)
+calc_gui.title('Calculator G.I.I.T.')
 
 expr = ""
 
@@ -64,7 +65,7 @@ def key_press(key):
 
 expr_input = StringVar()
 input_field = Entry(
-    calc,
+    calc_gui,
     font=('arial', 20, 'bold'),
     textvariable=expr_input,
 )
@@ -72,11 +73,11 @@ input_field.grid(columnspan=5, ipadx=1, ipady=1)
 input_field.focus()
 expr_input.get()
 
-calc.bind('<Key>', key_press)
+calc_gui.bind('<Key>', key_press)
 
 # First ROW
 button_nsqrt = tk.Button(
-    calc,
+    calc_gui,
     text="n√",
     height=2,
     width=4,
@@ -85,7 +86,7 @@ button_nsqrt = tk.Button(
 button_nsqrt.grid(row=1, column=0)
 
 button_sqrt = tk.Button(
-    calc,
+    calc_gui,
     text="√",
     height=2,
     width=4,
@@ -94,7 +95,7 @@ button_sqrt = tk.Button(
 button_sqrt.grid(row=1, column=1)
 
 button_pow = tk.Button(
-    calc,
+    calc_gui,
     text="x^2",
     height=2,
     width=4,
@@ -103,7 +104,7 @@ button_pow = tk.Button(
 button_pow.grid(row=1, column=2)
 
 button_pown = tk.Button(
-    calc,
+    calc_gui,
     text="x^n",
     height=2,
     width=4,
@@ -112,7 +113,7 @@ button_pown = tk.Button(
 button_pown.grid(row=1, column=3)
 
 button_smile = tk.Button(
-    calc,
+    calc_gui,
     text="smile",
     height=2,
     width=4,
@@ -122,7 +123,7 @@ button_smile.grid(row=1, column=4)
 
 # Second ROW
 button_factorial = tk.Button(
-    calc,
+    calc_gui,
     text="!",
     height=2,
     width=4,
@@ -131,7 +132,7 @@ button_factorial = tk.Button(
 button_factorial.grid(row=2, column=0)
 
 button_increment = tk.Button(
-    calc,
+    calc_gui,
     text="inc",
     height=2,
     width=4,
@@ -140,7 +141,7 @@ button_increment = tk.Button(
 button_increment.grid(row=2, column=1)
 
 button_decrement = tk.Button(
-    calc,
+    calc_gui,
     text="dec",
     height=2,
     width=4,
@@ -149,7 +150,7 @@ button_decrement = tk.Button(
 button_decrement.grid(row=2, column=2)
 
 button_clear = tk.Button(
-    calc,
+    calc_gui,
     text="C",
     height=2,
     width=4,
@@ -158,7 +159,7 @@ button_clear = tk.Button(
 button_clear.grid(row=2, column=3)
 
 button_delete = tk.Button(
-    calc,
+    calc_gui,
     text="<--",
     height=2,
     width=4,
@@ -168,7 +169,7 @@ button_delete.grid(row=2, column=4)
 
 # Third ROW
 button_plus = tk.Button(
-    calc,
+    calc_gui,
     text="+",
     height=2,
     width=4,
@@ -177,7 +178,7 @@ button_plus = tk.Button(
 button_plus.grid(row=3, column=0)
 
 button_7 = tk.Button(
-    calc,
+    calc_gui,
     text="7",
     height=2,
     width=4,
@@ -186,7 +187,7 @@ button_7 = tk.Button(
 button_7.grid(row=3, column=1)
 
 button_8 = tk.Button(
-    calc,
+    calc_gui,
     text="8",
     height=2,
     width=4,
@@ -195,7 +196,7 @@ button_8 = tk.Button(
 button_8.grid(row=3, column=2)
 
 button_9 = tk.Button(
-    calc,
+    calc_gui,
     text="9",
     height=2,
     width=4,
@@ -204,7 +205,7 @@ button_9 = tk.Button(
 button_9.grid(row=3, column=3)
 
 button_convert = tk.Button(
-    calc,
+    calc_gui,
     text="€->₽",
     height=2,
     width=4,
@@ -214,7 +215,7 @@ button_convert.grid(row=3, column=4)
 
 # Fourth ROW
 button_minus = tk.Button(
-    calc,
+    calc_gui,
     text="-",
     height=2,
     width=4,
@@ -223,7 +224,7 @@ button_minus = tk.Button(
 button_minus.grid(row=4, column=0)
 
 button_4 = tk.Button(
-    calc,
+    calc_gui,
     text="4",
     height=2,
     width=4,
@@ -232,7 +233,7 @@ button_4 = tk.Button(
 button_4.grid(row=4, column=1)
 
 button_5 = tk.Button(
-    calc,
+    calc_gui,
     text="5",
     height=2,
     width=4,
@@ -241,7 +242,7 @@ button_5 = tk.Button(
 button_5.grid(row=4, column=2)
 
 button_6 = tk.Button(
-    calc,
+    calc_gui,
     text="6",
     height=2,
     width=4,
@@ -250,7 +251,7 @@ button_6 = tk.Button(
 button_6.grid(row=4, column=3)
 
 button_rng = tk.Button(
-    calc,
+    calc_gui,
     text="RNG",
     height=2,
     width=4,
@@ -260,7 +261,7 @@ button_rng.grid(row=4, column=4)
 
 # Fifth ROW
 button_mul = tk.Button(
-    calc,
+    calc_gui,
     text="*",
     height=2,
     width=4,
@@ -269,7 +270,7 @@ button_mul = tk.Button(
 button_mul.grid(row=5, column=0)
 
 button_1 = tk.Button(
-    calc,
+    calc_gui,
     text="1",
     height=2,
     width=4,
@@ -278,7 +279,7 @@ button_1 = tk.Button(
 button_1.grid(row=5, column=1)
 
 button_2 = tk.Button(
-    calc,
+    calc_gui,
     text="2",
     height=2,
     width=4,
@@ -287,7 +288,7 @@ button_2 = tk.Button(
 button_2.grid(row=5, column=2)
 
 button_3 = tk.Button(
-    calc,
+    calc_gui,
     text="3",
     height=2,
     width=4,
@@ -296,7 +297,7 @@ button_3 = tk.Button(
 button_3.grid(row=5, column=3)
 
 button_equals = tk.Button(
-    calc,
+    calc_gui,
     text="=",
     height=5,
     width=4,
@@ -306,7 +307,7 @@ button_equals.grid(row=5, column=4, rowspan=2)
 
 # Sixth ROW
 button_div = tk.Button(
-    calc,
+    calc_gui,
     text="/",
     height=2,
     width=4,
@@ -315,7 +316,7 @@ button_div = tk.Button(
 button_div.grid(row=6, column=0)
 
 button_0 = tk.Button(
-    calc,
+    calc_gui,
     text="0",
     height=2,
     width=12,
@@ -325,7 +326,7 @@ button_0.grid(row=6, column=1, columnspan=2)
 
 
 button_dot = tk.Button(
-    calc,
+    calc_gui,
     text=".",
     height=2,
     width=4,
@@ -334,4 +335,4 @@ button_dot = tk.Button(
 button_dot.grid(row=6, column=3)
 
 
-calc.mainloop()  # run loops for events (clicks, keypress)
+calc_gui.mainloop()  # run loops for events (clicks, keypress)
