d3630f8593aa2f65ba175f96bb2564b93d3eec02
Martin <xvalac12@stud.fit.vutbr.cz>
2022-04-10 11:34:40 +0200

    Change from using tk to themed tk.

--- src/gui.py
+++ src/gui.py
@@ -64,7 +64,7 @@ def key_press(key):
 
 
 expr_input = StringVar()
-input_field = Entry(
+input_field = ttk.Entry(
     calc_gui,
     font=('arial', 20, 'bold'),
     textvariable=expr_input,
@@ -76,259 +76,259 @@ expr_input.get()
 calc_gui.bind('<Key>', key_press)
 
 # First ROW
-button_nsqrt = tk.Button(
+button_nsqrt = ttk.Button(
     calc_gui,
     text="n√",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("√", 1)
 )
 button_nsqrt.grid(row=1, column=0)
 
-button_sqrt = tk.Button(
+button_sqrt = ttk.Button(
     calc_gui,
     text="√",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("√", 1)
 )
 button_sqrt.grid(row=1, column=1)
 
-button_pow = tk.Button(
+button_pow = ttk.Button(
     calc_gui,
     text="x^2",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("^2", 2)
 )
 button_pow.grid(row=1, column=2)
 
-button_pown = tk.Button(
+button_pown = ttk.Button(
     calc_gui,
     text="x^n",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("^2", 2)
 )
 button_pown.grid(row=1, column=3)
 
-button_smile = tk.Button(
+button_smile = ttk.Button(
     calc_gui,
     text="smile",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("lol", 3)
 )
 button_smile.grid(row=1, column=4)
 
 # Second ROW
-button_factorial = tk.Button(
+button_factorial = ttk.Button(
     calc_gui,
     text="!",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("!", 1)
 )
 button_factorial.grid(row=2, column=0)
 
-button_increment = tk.Button(
+button_increment = ttk.Button(
     calc_gui,
     text="inc",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("++", 2)
 )
 button_increment.grid(row=2, column=1)
 
-button_decrement = tk.Button(
+button_decrement = ttk.Button(
     calc_gui,
     text="dec",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("--", 2)
 )
 button_decrement.grid(row=2, column=2)
 
-button_clear = tk.Button(
+button_clear = ttk.Button(
     calc_gui,
     text="C",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: clear()
 )
 button_clear.grid(row=2, column=3)
 
-button_delete = tk.Button(
+button_delete = ttk.Button(
     calc_gui,
     text="<--",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: delete()
 )
 button_delete.grid(row=2, column=4)
 
 # Third ROW
-button_plus = tk.Button(
+button_plus = ttk.Button(
     calc_gui,
     text="+",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("+", 1)
 )
 button_plus.grid(row=3, column=0)
 
-button_7 = tk.Button(
+button_7 = ttk.Button(
     calc_gui,
     text="7",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("7", 1)
 )
 button_7.grid(row=3, column=1)
 
-button_8 = tk.Button(
+button_8 = ttk.Button(
     calc_gui,
     text="8",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("8", 1)
 )
 button_8.grid(row=3, column=2)
 
-button_9 = tk.Button(
+button_9 = ttk.Button(
     calc_gui,
     text="9",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("9", 1)
 )
 button_9.grid(row=3, column=3)
 
-button_convert = tk.Button(
+button_convert = ttk.Button(
     calc_gui,
     text="€->₽",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("√", 1)
 )
 button_convert.grid(row=3, column=4)
 
 # Fourth ROW
-button_minus = tk.Button(
+button_minus = ttk.Button(
     calc_gui,
     text="-",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("-", 1)
 )
 button_minus.grid(row=4, column=0)
 
-button_4 = tk.Button(
+button_4 = ttk.Button(
     calc_gui,
     text="4",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("4", 1)
 )
 button_4.grid(row=4, column=1)
 
-button_5 = tk.Button(
+button_5 = ttk.Button(
     calc_gui,
     text="5",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("5", 1)
 )
 button_5.grid(row=4, column=2)
 
-button_6 = tk.Button(
+button_6 = ttk.Button(
     calc_gui,
     text="6",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("6", 1)
 )
 button_6.grid(row=4, column=3)
 
-button_rng = tk.Button(
+button_rng = ttk.Button(
     calc_gui,
     text="RNG",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("√", 1)
 )
 button_rng.grid(row=4, column=4)
 
 # Fifth ROW
-button_mul = tk.Button(
+button_mul = ttk.Button(
     calc_gui,
     text="*",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("*", 1)
 )
 button_mul.grid(row=5, column=0)
 
-button_1 = tk.Button(
+button_1 = ttk.Button(
     calc_gui,
     text="1",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("1", 1)
 )
 button_1.grid(row=5, column=1)
 
-button_2 = tk.Button(
+button_2 = ttk.Button(
     calc_gui,
     text="2",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("2", 1)
 )
 button_2.grid(row=5, column=2)
 
-button_3 = tk.Button(
+button_3 = ttk.Button(
     calc_gui,
     text="3",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("3", 1)
 )
 button_3.grid(row=5, column=3)
 
-button_equals = tk.Button(
+button_equals = ttk.Button(
     calc_gui,
     text="=",
-    height=5,
+    # height=5,
     width=4,
     command=lambda: equals()
 )
 button_equals.grid(row=5, column=4, rowspan=2)
 
 # Sixth ROW
-button_div = tk.Button(
+button_div = ttk.Button(
     calc_gui,
     text="/",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press("/", 1)
 )
 button_div.grid(row=6, column=0)
 
-button_0 = tk.Button(
+button_0 = ttk.Button(
     calc_gui,
     text="0",
-    height=2,
+    # height=2,
     width=12,
     command=lambda: button_press("0", 1)
 )
 button_0.grid(row=6, column=1, columnspan=2)
 
 
-button_dot = tk.Button(
+button_dot = ttk.Button(
     calc_gui,
     text=".",
-    height=2,
+    # height=2,
     width=4,
     command=lambda: button_press(".", 1)
 )
