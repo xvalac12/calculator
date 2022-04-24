f1626b2e9a299e5def7c451dddcffaab0db7dea2
Samuel <samuelstolko@gmail.com>
2022-04-24 23:14:26 +0200

    added get_input and fixed zero division in avg

--- src/profiling.py
+++ src/profiling.py
@@ -8,10 +8,21 @@
 ##
 
 from functions import power, root
+from math import sqrt
 
+def get_input() -> list:
+    try:
+        input_numbers = []
+        while(True):
+            #until eof, then input() raises exception
+            input_numbers += list(map(int,input().split())) 
+    except:
+        return input_numbers
 
 def avg(numbers: list) -> int:
-
+    print(numbers)
+    if len(numbers) == 0:
+        return int(0)
+    
     return sum(numbers)/len(numbers)
 
-print(avg([4,2]))
