ea05525f391b30c1acebbd7e8256d205f04d1479
Samuel <samuelstolko@gmail.com>
2022-04-25 18:25:21 +0200

    sorting by tottime

--- profiling/profile.sh
+++ profiling/profile.sh
@@ -3,10 +3,10 @@
 #Has to be run from inside profiling folder
 
 echo "Running profiling on 10 input numbers" > vystup.out
-seq 1000 1000000 | shuf | head -10 | python3 -m cProfile -s cumtime ../src/profiling.py >> vystup.out
+seq 1000 1000000 | shuf | head -10 | python3 -m cProfile -s tottime ../src/profiling.py >> vystup.out
 
 echo "Running profiling on 100 input numbers" >> vystup.out
-seq 1000 1000000 | shuf | head -100 | python3 -m cProfile -s cumtime ../src/profiling.py >> vystup.out
+seq 1000 1000000 | shuf | head -100 | python3 -m cProfile -s tottime ../src/profiling.py >> vystup.out
 
 echo "Running profiling on 1000 input numbers" >> vystup.out
-seq 1000 1000000 | shuf | head -1000 | python3 -m cProfile -s cumtime ../src/profiling.py >> vystup.out
+seq 1000 1000000 | shuf | head -1000 | python3 -m cProfile -s tottime ../src/profiling.py >> vystup.out
