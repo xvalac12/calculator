f49f4ace3408b979ed622e62b9feed5d25df30f8
Samuel <samuelstolko@gmail.com>
2022-04-26 18:13:59 +0200

    profiled on 1 000 000 numbers

--- profiling/profile.sh
+++ profiling/profile.sh
@@ -10,3 +10,6 @@ seq 1000 1000000 | shuf | head -100 | python3 -m cProfile -s tottime ../src/prof
 
 echo "Running profiling on 1000 input numbers" >> vystup.out
 seq 1000 1000000 | shuf | head -1000 | python3 -m cProfile -s tottime ../src/profiling.py >> vystup.out
+
+echo "Running profiling on 1000 000 input numbers" >> vystup.out
+seq 1000 10000000 | shuf | head -1000000 | python3 -m cProfile -s tottime ../src/profiling.py >> vystup.out
