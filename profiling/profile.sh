74a5f3f150809b78a8f29ea72f8c883013fe3d41
Samuel <samuelstolko@gmail.com>
2022-04-27 12:51:33 +0200

    not fixed output file

--- profiling/profile.sh
+++ profiling/profile.sh
@@ -2,14 +2,14 @@
 
 #Has to be run from inside profiling folder
 
-echo "Running profiling on 10 input numbers" > vystup.out
-seq 1000 1000000 | shuf | head -10 | python3 -m cProfile -s tottime ../src/profiling.py >> vystup.out
+echo "Running profiling on 10 input numbers" 
+seq 1000 1000000 | shuf | head -10 | python3 -m cProfile -s tottime ../src/profiling.py 
 
-echo "Running profiling on 100 input numbers" >> vystup.out
-seq 1000 1000000 | shuf | head -100 | python3 -m cProfile -s tottime ../src/profiling.py >> vystup.out
+echo "Running profiling on 100 input numbers" 
+seq 1000 1000000 | shuf | head -100 | python3 -m cProfile -s tottime ../src/profiling.py 
 
-echo "Running profiling on 1000 input numbers" >> vystup.out
-seq 1000 1000000 | shuf | head -1000 | python3 -m cProfile -s tottime ../src/profiling.py >> vystup.out
+echo "Running profiling on 1000 input numbers" 
+seq 1000 1000000 | shuf | head -1000 | python3 -m cProfile -s tottime ../src/profiling.py 
 
-echo "Running profiling on 1000 000 input numbers" >> vystup.out
-seq 1000 10000000 | shuf | head -1000000 | python3 -m cProfile -s tottime ../src/profiling.py >> vystup.out
+echo "Running profiling on 1000 000 input numbers" 
+seq 1000 10000000 | shuf | head -1000000 | python3 -m cProfile -s tottime ../src/profiling.py 
