085cf2439a91a88235dc2447149f636c74787db3
Martin <xvalac12@stud.fit.vutbr.cz>
2022-04-28 11:27:24 +0200

    Added frame for doxygen + Fixed formatting and typos

--- src/profiling.py
+++ src/profiling.py
@@ -1,43 +1,77 @@
 #!/usr/bin/python3
 
 ##
+# @section description_gui Description
+# Defines GUI of calculator app and behavior of buttons.
+#
+# @section libraries_gui Libraries/Modules
+# - cmath python library
+# - math python library
+#
 # @file profiling.py
-# @author Samuel Stolarik
-# @date 2022-04-24
-# @brief mathematical function to be profiled
-##
+# @brief
+# @author Samuel Stolárik <xstola02@stud.fit.vutbr.cz>
+# @date 28.4.2022
 
 from cmath import nan
 from functions import power, root
 from math import sqrt
 
+
 def get_input() -> list:
+    """!
+    @brief short description
+    @return
+
+    longer description (if needed)
+    """
     try:
         input_numbers = []
-        while(True):
-            #until eof, then input() raises exception
-            input_numbers += list(map(int,input().split())) 
+        while True:
+            # until eof, then input() raises exception
+            input_numbers += list(map(int, input().split())) 
     except:
         return input_numbers
 
 
 def avg(numbers: list) -> int:
+    """!
+    @brief short description
+    @param numbers
+    @return
+
+    longer description (if needed)
+    """
     if len(numbers) == 0:
         return int(0)
     return sum(numbers)/len(numbers)
 
 
-def inside_function(numbers:list):
+def inside_function(numbers: list):
+    """!
+    @brief short description
+    @param numbers
+    @return
+
+    longer description (if needed)
+    """
     sum = 0
     for i in numbers:
-        sum += power(i,2)
+        sum += power(i, 2)
         
-    sum-=len(numbers) * power(avg(numbers), 2)
+    sum -= len(numbers) * power(avg(numbers), 2)
     
     return sum
 
 
-def expresion(numbers:list):
+def expression(numbers: list):
+    """!
+    @brief short description
+    @param numbers
+    @return
+
+    longer description (if needed)
+    """
     try:
         partial = inside_function(numbers) / (len(numbers) - 1)
 
@@ -49,10 +83,10 @@ def expresion(numbers:list):
         return root(partial, 2)
 
     except ValueError:
-        print("ERROR: Sqrt of negative number. Number: ", partial,".")
+        print("ERROR: Sqrt of negative number. Number: ", partial, ".")
         return nan
     
 
 if __name__ == "__main__":
     input_numbers = get_input()
-    print(expresion(input_numbers))
+    print(expression(input_numbers))
